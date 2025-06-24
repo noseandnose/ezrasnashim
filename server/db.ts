a91c02e07346011ea58a48ffc8a300121076c9ca
jz <jonathan@stilyoapps.com>
2025-06-24 16:27:12 +0300

    don't force ssl

--- server/db.ts
+++ server/db.ts
@@ -11,9 +11,9 @@ if (!process.env.DATABASE_URL) {
 // Supabase connection configuration
 export const pool = new Pool({ 
   connectionString: process.env.DATABASE_URL,
-  ssl: { 
-    rejectUnauthorized: false 
-  },
+  // ssl: {
+  //   rejectUnauthorized: false
+  // },
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
